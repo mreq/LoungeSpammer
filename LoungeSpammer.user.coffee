6aada8773198fb2b20b4b3540755c2e67310021e
Petr Marek <contact@petrmarek.eu>
2014-08-26 00:26:04 +0200

    1st release

--- LoungeSpammer.user.coffee
+++ LoungeSpammer.user.coffee
@@ -1,46 +1,70 @@
+# coffee-no-wrap
+
 class Spammer
 	constructor: ->
-		@createBox()
 		@overwriteAlert()
 		@addButton()
 	createBox: ->
-		@box = $('<section class="box"><div class="title">Status</div><article class="standard" style="margin-top:50px;" id="loungespammer"></article></section>')
+		@box = $("""
+			<section class="box">
+				<div class="title">Status</div>
+					<article class="standard" style="margin-top: 50px; margin-bottom: 0;" id="loungespammer"></article>
+					<article class="standard" style="margin-top: 19px;"><em>Does this script help you?</em> Be kind and say <a href="http://steamcommunity.com/tradeoffer/new/?partner=16222794&token=BOqXsQwB" target="_blank">thanks to the author</a> &lt;3.</article>
+			</section>
+		""")
 		$('#main > .box').first().after @box
 		@content = @box.find('#loungespammer')
 	overwriteAlert: ->
 		@i = 0
+		@messages = []
 		window.alert = (c) =>
-			@content.text "#{ @i }: #{ c }"
+			@messages.push c  if @messages.indexOf(c) is -1
+			@content.html """
+				<strong>#{ c }</strong><br><br>
+				Tried #{ @i } times over the last #{ Math.floor((Date.now()-@startTime)/60000) } minute(s).<br><br>
+				Messages so far:<br>
+				<ul style="padding-left: 30px;">
+					<li style="padding-top: 5px;">#{ @messages.join('<li style="padding-top: 5px;"></li>') }</li>
+				</ul>
+			"""
 	startSpamming: ->
-		setInterval (=> @i++; @spamLounge()), 1500
+		@startTime = Date.now()
+		@createBox()
+		@spamLounge()
+		setInterval (=> @spamLounge()), 1500
 	addButton: ->
-		@spamButton.one 'click', @startSpamming
+		@spamButton.one 'click', => @startSpamming()
 	spamLounge: ->
+		@i++
+		@button.click()
 
 class BetSpammer extends Spammer
-	spamLounge: postToFreezeReturn
-	addButton: ->
-		@spamButton = $('<a class="button" id="loungespammer-button">Spam place bet</a>')
-		$('#freezebutton').after @spamButton
-		super
-
-class ReturnsSpammer extends Spammer
 	constructor: ->
 		@button = $('#placebut')
 		super
-	spamLounge: ->
-		@button.click()
 	addButton: ->
-		@spamButton = $('<a class="button" id="loungespammer-button">Spam request returns</a>')
-		$('#freezebutton').after @spamButton
+		@spamButton = $('<a class="buttonright" style="margin-left: 5px;" id="loungespammer-button">Spam place bet</a>')
 		super
+		@button.before @spamButton
+
+class ReturnsSpammer extends Spammer
+	constructor: ->
+		@button = $('#freezebutton')
+		super
+	addButton: ->
+		@spamButton = $('<a class="button" style="margin-left: 5px;" id="loungespammer-button">Spam request returns</a>')
+		super
+		@button.after @spamButton
+	startSpamming: ->
+		super
+		@spamLounge()
 
 
 init = ->
 	url = window.location.href
-	if url.match 'mybets' isnt null
+	if url.match('mybets') isnt null
 		new ReturnsSpammer
-	else if url.match 'match' isnt null
+	else if url.match('match') isnt null
 		new BetSpammer
 
-$ -> init()
+init()
--- LoungeSpammer.user.js
+++ LoungeSpammer.user.js
@@ -4,113 +4,121 @@
 // @author      mreq http://github.com/mreq
 // @include     http://csgolounge.com/mybets
 // @include     http://dota2lounge.com/mybets
-// @include     http://csgolounge.com/match
-// @include     http://dota2lounge.com/match
-// @version     0.0.1
+// @include     http://csgolounge.com/match?m=*
+// @include     http://dota2lounge.com/match?m=*
+// @version     0.1.0
+// @require     //ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js
 // @downloadURL http://cdn.mreq.eu/LoungeSpammer.user.js
 // @updateURL   http://cdn.mreq.eu/LoungeSpammer.user.js
 // @grant       none
 // ==/UserScript==
 
-(function() {
-  var BetSpammer, ReturnsSpammer, Spammer, init,
-    __hasProp = {}.hasOwnProperty,
-    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+var BetSpammer, ReturnsSpammer, Spammer, init,
+  __hasProp = {}.hasOwnProperty,
+  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
-  Spammer = (function() {
-    function Spammer() {
-      this.createBox();
-      this.overwriteAlert();
-      this.addButton();
-    }
+Spammer = (function() {
+  function Spammer() {
+    this.overwriteAlert();
+    this.addButton();
+  }
 
-    Spammer.prototype.createBox = function() {
-      this.box = $('<section class="box"><div class="title">Status</div><article class="standard" style="margin-top:50px;" id="loungespammer"></article></section>');
-      $('#main > .box').first().after(this.box);
-      return this.content = this.box.find('#loungespammer');
-    };
-
-    Spammer.prototype.overwriteAlert = function() {
-      this.i = 0;
-      return window.alert = (function(_this) {
-        return function(c) {
-          return _this.content.text("" + _this.i + ": " + c);
-        };
-      })(this);
-    };
-
-    Spammer.prototype.startSpamming = function() {
-      return setInterval(((function(_this) {
-        return function() {
-          _this.i++;
-          return _this.spamLounge();
-        };
-      })(this)), 1500);
-    };
-
-    Spammer.prototype.addButton = function() {
-      return this.spamButton.one('click', this.startSpamming);
-    };
-
-    Spammer.prototype.spamLounge = function() {};
-
-    return Spammer;
-
-  })();
-
-  BetSpammer = (function(_super) {
-    __extends(BetSpammer, _super);
-
-    function BetSpammer() {
-      return BetSpammer.__super__.constructor.apply(this, arguments);
-    }
-
-    BetSpammer.prototype.spamLounge = postToFreezeReturn;
-
-    BetSpammer.prototype.addButton = function() {
-      this.spamButton = $('<a class="button" id="loungespammer-button">Spam place bet</a>');
-      $('#freezebutton').after(this.spamButton);
-      return BetSpammer.__super__.addButton.apply(this, arguments);
-    };
-
-    return BetSpammer;
-
-  })(Spammer);
-
-  ReturnsSpammer = (function(_super) {
-    __extends(ReturnsSpammer, _super);
-
-    function ReturnsSpammer() {
-      this.button = $('#placebut');
-      ReturnsSpammer.__super__.constructor.apply(this, arguments);
-    }
-
-    ReturnsSpammer.prototype.spamLounge = function() {
-      return this.button.click();
-    };
-
-    ReturnsSpammer.prototype.addButton = function() {
-      this.spamButton = $('<a class="button" id="loungespammer-button">Spam request returns</a>');
-      $('#freezebutton').after(this.spamButton);
-      return ReturnsSpammer.__super__.addButton.apply(this, arguments);
-    };
-
-    return ReturnsSpammer;
-
-  })(Spammer);
-
-  init = function() {
-    var url;
-    url = window.location.href;
-    if (url.match('mybets' !== null)) {
-      return new ReturnsSpammer;
-    } else if (url.match('match' !== null)) {
-      return new BetSpammer;
-    }
+  Spammer.prototype.createBox = function() {
+    this.box = $("<section class=\"box\">\n	<div class=\"title\">Status</div>\n		<article class=\"standard\" style=\"margin-top: 50px; margin-bottom: 0;\" id=\"loungespammer\"></article>\n		<article class=\"standard\" style=\"margin-top: 19px;\"><em>Does this script help you?</em> Be kind and say <a href=\"http://steamcommunity.com/tradeoffer/new/?partner=16222794&token=BOqXsQwB\" target=\"_blank\">thanks to the author</a> &lt;3.</article>\n</section>");
+    $('#main > .box').first().after(this.box);
+    return this.content = this.box.find('#loungespammer');
   };
 
-  $(function() {
-    return init();
-  });
+  Spammer.prototype.overwriteAlert = function() {
+    this.i = 0;
+    this.messages = [];
+    return window.alert = (function(_this) {
+      return function(c) {
+        if (_this.messages.indexOf(c) === -1) {
+          _this.messages.push(c);
+        }
+        return _this.content.html("<strong>" + c + "</strong><br><br>\nTried " + _this.i + " times over the last " + (Math.floor((Date.now() - _this.startTime) / 60000)) + " minute(s).<br><br>\nMessages so far:<br>\n<ul style=\"padding-left: 30px;\">\n	<li style=\"padding-top: 5px;\">" + (_this.messages.join('<li style="padding-top: 5px;"></li>')) + "</li>\n</ul>");
+      };
+    })(this);
+  };
 
-}).call(this);
+  Spammer.prototype.startSpamming = function() {
+    this.startTime = Date.now();
+    this.createBox();
+    this.spamLounge();
+    return setInterval(((function(_this) {
+      return function() {
+        return _this.spamLounge();
+      };
+    })(this)), 1500);
+  };
+
+  Spammer.prototype.addButton = function() {
+    return this.spamButton.one('click', (function(_this) {
+      return function() {
+        return _this.startSpamming();
+      };
+    })(this));
+  };
+
+  Spammer.prototype.spamLounge = function() {
+    this.i++;
+    return this.button.click();
+  };
+
+  return Spammer;
+
+})();
+
+BetSpammer = (function(_super) {
+  __extends(BetSpammer, _super);
+
+  function BetSpammer() {
+    this.button = $('#placebut');
+    BetSpammer.__super__.constructor.apply(this, arguments);
+  }
+
+  BetSpammer.prototype.addButton = function() {
+    this.spamButton = $('<a class="buttonright" style="margin-left: 5px;" id="loungespammer-button">Spam place bet</a>');
+    BetSpammer.__super__.addButton.apply(this, arguments);
+    return this.button.before(this.spamButton);
+  };
+
+  return BetSpammer;
+
+})(Spammer);
+
+ReturnsSpammer = (function(_super) {
+  __extends(ReturnsSpammer, _super);
+
+  function ReturnsSpammer() {
+    this.button = $('#freezebutton');
+    ReturnsSpammer.__super__.constructor.apply(this, arguments);
+  }
+
+  ReturnsSpammer.prototype.addButton = function() {
+    this.spamButton = $('<a class="button" style="margin-left: 5px;" id="loungespammer-button">Spam request returns</a>');
+    ReturnsSpammer.__super__.addButton.apply(this, arguments);
+    return this.button.after(this.spamButton);
+  };
+
+  ReturnsSpammer.prototype.startSpamming = function() {
+    ReturnsSpammer.__super__.startSpamming.apply(this, arguments);
+    return this.spamLounge();
+  };
+
+  return ReturnsSpammer;
+
+})(Spammer);
+
+init = function() {
+  var url;
+  url = window.location.href;
+  if (url.match('mybets') !== null) {
+    return new ReturnsSpammer;
+  } else if (url.match('match') !== null) {
+    return new BetSpammer;
+  }
+};
+
+init();
