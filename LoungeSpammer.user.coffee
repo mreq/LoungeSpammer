bf11739620259c9daba1d84206bbd65370af3e24
Petr Marek <contact@petrmarek.eu>
2014-08-26 11:29:54 +0200

    bot status checker

--- LoungeSpammer.user.coffee
+++ LoungeSpammer.user.coffee
@@ -1,12 +1,78 @@
 # coffee-no-wrap
 
-class Spammer
+class Base
+	constructor: ->
+
+	initNotifications: ->
+		if not ('Notification' of window)
+			@notify = ->
+				console.log 'Notifications not supported.'
+		else if (window.Notification.permission isnt 'granted') and (window.Notification.permission isnt 'denied')
+			@permitNotifications()
+	permitNotifications: ->
+		window.Notification.requestPermission (p) =>
+			unless ('permission' of window.Notification)
+			  window.Notification.permission = p
+			unless p is 'granted'
+				@notify = ->
+					console.log 'Notifications not allowed.'
+			@notify 'Success! LoungeSpammer can now use notifications.'
+	notify: (body) ->
+		options =
+			body: body
+			icon: 'http://i.imgur.com/bWiPLuy.png'
+		new window.Notification 'LoungeSpammer', options
+
+class BotsChecker extends Base
+	constructor: ->
+		@createBox()
+		@check()
+		@autoChecking = false
+		@bindButton()
+	createBox: ->
+		@wrap = $('<div style="padding: 8px 10px;">Bots status: <div id="loungespammer-status" style="color: green; font-weight: bold; font-size: 130%; width: 100%;">&nbsp;</div><div id="loungespammer-status-spam" style="display: none; width: 100%; cursor: pointer; color: #FF8A00;">Notify me when they go online!</div><div style="width: 100%;"><em>Does this script help you?</em> Be kind and say <a href="http://steamcommunity.com/tradeoffer/new/?partner=16222794&token=BOqXsQwB" target="_blank" style="background: none; padding: 0; display: inline; float: none; text-decoration: none; border: 0;">thanks to the author</a> &lt;3.</div></div>')
+		$('#submenu').prepend @wrap
+		@content = @wrap.find('#loungespammer-status')
+		@button = @wrap.find('#loungespammer-status-spam')
+	check: ->
+		$.get 'status', (data) =>
+			old = @online
+			@online = (data.match('BOTS ARE ONLINE') isnt null)
+			@correctContent()  unless old is @online
+			if @autoChecking and @online
+				clearInterval(@interval)
+				@notify 'Bots are now online!'
+	autoCheck: ->
+		@autoChecking = true
+		@i = 0
+		@interval = setInterval (=>
+			@i++
+			@check()
+			@button.text "Bots still offline, checking every 30 seconds. Last checked at #{ Date().match(/\d+:\d+:\d+/)[0] }."
+		), 30000
+
+		@i++
+		@check()
+		@button.css('color', '').text "Bots still offline, checking every 30 seconds. Last checked at #{ Date().match(/\d+:\d+:\d+/)[0] }."
+	bindButton: ->
+		@button.one 'click', =>
+			@initNotifications()
+			@autoCheck()
+	correctContent: ->
+		if @online
+			@content.text('ONLINE').css('color', 'green')
+			@button.hide()
+		else
+			@content.text('OFFLINE').css('color', 'red')
+			@button.show()
+
+class Spammer extends Base
 	constructor: ->
 		@overwriteAlert()
 		@addButton()
 	createBox: ->
 		@box = $("""
-			<section class="box">
+			<section class="box" style="display: none;">
 				<div class="title">Status</div>
 					<article class="standard" style="margin-top: 50px; margin-bottom: 0;" id="loungespammer"></article>
 					<article class="standard" style="margin-top: 19px;"><em>Does this script help you?</em> Be kind and say <a href="http://steamcommunity.com/tradeoffer/new/?partner=16222794&token=BOqXsQwB" target="_blank">thanks to the author</a> &lt;3.</article>
@@ -17,7 +83,9 @@ class Spammer
 	overwriteAlert: ->
 		@i = 0
 		@messages = []
+		@createBox()
 		window.alert = (c) =>
+			@box.show()
 			@messages.push c  if @messages.indexOf(c) is -1
 			@content.html """
 				<strong>#{ c }</strong><br><br>
@@ -29,11 +97,12 @@ class Spammer
 			"""
 	startSpamming: ->
 		@startTime = Date.now()
-		@createBox()
 		@spamLounge()
 		setInterval (=> @spamLounge()), 1500
 	addButton: ->
-		@spamButton.one 'click', => @startSpamming()
+		@spamButton.one 'click', =>
+			@initNotifications()
+			@startSpamming()
 	spamLounge: ->
 		@i++
 		@button.click()
@@ -46,6 +115,14 @@ class BetSpammer extends Spammer
 		@spamButton = $('<a class="buttonright" style="margin-left: 5px;" id="loungespammer-button">Spam place bet</a>')
 		super
 		@button.before @spamButton
+	startSpamming: ->
+		super
+		$(document).on 'ajaxSuccess', (a, b, c) =>
+			unless c.data
+				if c.url is 'ajax/postBet.php'
+					@notify 'Placed bet.'
+				else if c.url is 'ajax/postBetOffer.php'
+					@notify 'Got a place bet trade offer.'
 
 class ReturnsSpammer extends Spammer
 	constructor: ->
@@ -57,6 +134,10 @@ class ReturnsSpammer extends Spammer
 		@button.after @spamButton
 	startSpamming: ->
 		super
+		$(document).on 'ajaxSuccess', (a, b, c) =>
+			unless c.data
+				if c.url is 'ajax/postToReturn.php'
+					@notify 'Requested returns ready.'
 		@spamLounge()
 
 
@@ -66,5 +147,7 @@ init = ->
 		new ReturnsSpammer
 	else if url.match('match') isnt null
 		new BetSpammer
+
+	new BotsChecker
 
 init()
--- LoungeSpammer.user.js
+++ LoungeSpammer.user.js
@@ -2,28 +2,142 @@
 // @name        LoungeSpammer
 // @namespace   LoungeSpammer
 // @author      mreq http://github.com/mreq
-// @include     http://csgolounge.com/mybets
-// @include     http://dota2lounge.com/mybets
-// @include     http://csgolounge.com/match?m=*
-// @include     http://dota2lounge.com/match?m=*
-// @version     0.1.0
+// @include     http://csgolounge.com/*
+// @include     http://dota2lounge.com/*
+// @version     0.2.2
 // @downloadURL http://cdn.mreq.eu/LoungeSpammer.user.js
 // @updateURL   http://cdn.mreq.eu/LoungeSpammer.user.js
 // @grant       none
 // ==/UserScript==
 
-var BetSpammer, ReturnsSpammer, Spammer, init,
+var Base, BetSpammer, BotsChecker, ReturnsSpammer, Spammer, init,
   __hasProp = {}.hasOwnProperty,
   __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
-Spammer = (function() {
+Base = (function() {
+  function Base() {}
+
+  Base.prototype.initNotifications = function() {
+    if (!('Notification' in window)) {
+      return this.notify = function() {
+        return console.log('Notifications not supported.');
+      };
+    } else if ((window.Notification.permission !== 'granted') && (window.Notification.permission !== 'denied')) {
+      return this.permitNotifications();
+    }
+  };
+
+  Base.prototype.permitNotifications = function() {
+    return window.Notification.requestPermission((function(_this) {
+      return function(p) {
+        if (!('permission' in window.Notification)) {
+          window.Notification.permission = p;
+        }
+        if (p !== 'granted') {
+          _this.notify = function() {
+            return console.log('Notifications not allowed.');
+          };
+        }
+        return _this.notify('Success! LoungeSpammer can now use notifications.');
+      };
+    })(this));
+  };
+
+  Base.prototype.notify = function(body) {
+    var options;
+    options = {
+      body: body,
+      icon: 'http://i.imgur.com/bWiPLuy.png'
+    };
+    return new window.Notification('LoungeSpammer', options);
+  };
+
+  return Base;
+
+})();
+
+BotsChecker = (function(_super) {
+  __extends(BotsChecker, _super);
+
+  function BotsChecker() {
+    this.createBox();
+    this.check();
+    this.autoChecking = false;
+    this.bindButton();
+  }
+
+  BotsChecker.prototype.createBox = function() {
+    this.wrap = $('<div style="padding: 8px 10px;">Bots status: <div id="loungespammer-status" style="color: green; font-weight: bold; font-size: 130%; width: 100%;">&nbsp;</div><div id="loungespammer-status-spam" style="display: none; width: 100%; cursor: pointer; color: #FF8A00;">Notify me when they go online!</div><div style="width: 100%;"><em>Does this script help you?</em> Be kind and say <a href="http://steamcommunity.com/tradeoffer/new/?partner=16222794&token=BOqXsQwB" target="_blank" style="background: none; padding: 0; display: inline; float: none; text-decoration: none; border: 0;">thanks to the author</a> &lt;3.</div></div>');
+    $('#submenu').prepend(this.wrap);
+    this.content = this.wrap.find('#loungespammer-status');
+    return this.button = this.wrap.find('#loungespammer-status-spam');
+  };
+
+  BotsChecker.prototype.check = function() {
+    return $.get('status', (function(_this) {
+      return function(data) {
+        var old;
+        old = _this.online;
+        _this.online = data.match('BOTS ARE ONLINE') !== null;
+        if (old !== _this.online) {
+          _this.correctContent();
+        }
+        if (_this.autoChecking && _this.online) {
+          clearInterval(_this.interval);
+          return _this.notify('Bots are now online!');
+        }
+      };
+    })(this));
+  };
+
+  BotsChecker.prototype.autoCheck = function() {
+    this.autoChecking = true;
+    this.i = 0;
+    this.interval = setInterval(((function(_this) {
+      return function() {
+        _this.i++;
+        _this.check();
+        return _this.button.text("Bots still offline, checking every 30 seconds. Last checked at " + (Date().match(/\d+:\d+:\d+/)[0]) + ".");
+      };
+    })(this)), 30000);
+    this.i++;
+    this.check();
+    return this.button.css('color', '').text("Bots still offline, checking every 30 seconds. Last checked at " + (Date().match(/\d+:\d+:\d+/)[0]) + ".");
+  };
+
+  BotsChecker.prototype.bindButton = function() {
+    return this.button.one('click', (function(_this) {
+      return function() {
+        _this.initNotifications();
+        return _this.autoCheck();
+      };
+    })(this));
+  };
+
+  BotsChecker.prototype.correctContent = function() {
+    if (this.online) {
+      this.content.text('ONLINE').css('color', 'green');
+      return this.button.hide();
+    } else {
+      this.content.text('OFFLINE').css('color', 'red');
+      return this.button.show();
+    }
+  };
+
+  return BotsChecker;
+
+})(Base);
+
+Spammer = (function(_super) {
+  __extends(Spammer, _super);
+
   function Spammer() {
     this.overwriteAlert();
     this.addButton();
   }
 
   Spammer.prototype.createBox = function() {
-    this.box = $("<section class=\"box\">\n	<div class=\"title\">Status</div>\n		<article class=\"standard\" style=\"margin-top: 50px; margin-bottom: 0;\" id=\"loungespammer\"></article>\n		<article class=\"standard\" style=\"margin-top: 19px;\"><em>Does this script help you?</em> Be kind and say <a href=\"http://steamcommunity.com/tradeoffer/new/?partner=16222794&token=BOqXsQwB\" target=\"_blank\">thanks to the author</a> &lt;3.</article>\n</section>");
+    this.box = $("<section class=\"box\" style=\"display: none;\">\n	<div class=\"title\">Status</div>\n		<article class=\"standard\" style=\"margin-top: 50px; margin-bottom: 0;\" id=\"loungespammer\"></article>\n		<article class=\"standard\" style=\"margin-top: 19px;\"><em>Does this script help you?</em> Be kind and say <a href=\"http://steamcommunity.com/tradeoffer/new/?partner=16222794&token=BOqXsQwB\" target=\"_blank\">thanks to the author</a> &lt;3.</article>\n</section>");
     $('#main > .box').first().after(this.box);
     return this.content = this.box.find('#loungespammer');
   };
@@ -31,8 +145,10 @@ Spammer = (function() {
   Spammer.prototype.overwriteAlert = function() {
     this.i = 0;
     this.messages = [];
+    this.createBox();
     return window.alert = (function(_this) {
       return function(c) {
+        _this.box.show();
         if (_this.messages.indexOf(c) === -1) {
           _this.messages.push(c);
         }
@@ -43,7 +159,6 @@ Spammer = (function() {
 
   Spammer.prototype.startSpamming = function() {
     this.startTime = Date.now();
-    this.createBox();
     this.spamLounge();
     return setInterval(((function(_this) {
       return function() {
@@ -55,6 +170,7 @@ Spammer = (function() {
   Spammer.prototype.addButton = function() {
     return this.spamButton.one('click', (function(_this) {
       return function() {
+        _this.initNotifications();
         return _this.startSpamming();
       };
     })(this));
@@ -67,7 +183,7 @@ Spammer = (function() {
 
   return Spammer;
 
-})();
+})(Base);
 
 BetSpammer = (function(_super) {
   __extends(BetSpammer, _super);
@@ -81,6 +197,21 @@ BetSpammer = (function(_super) {
     this.spamButton = $('<a class="buttonright" style="margin-left: 5px;" id="loungespammer-button">Spam place bet</a>');
     BetSpammer.__super__.addButton.apply(this, arguments);
     return this.button.before(this.spamButton);
+  };
+
+  BetSpammer.prototype.startSpamming = function() {
+    BetSpammer.__super__.startSpamming.apply(this, arguments);
+    return $(document).on('ajaxSuccess', (function(_this) {
+      return function(a, b, c) {
+        if (!c.data) {
+          if (c.url === 'ajax/postBet.php') {
+            return _this.notify('Placed bet.');
+          } else if (c.url === 'ajax/postBetOffer.php') {
+            return _this.notify('Got a place bet trade offer.');
+          }
+        }
+      };
+    })(this));
   };
 
   return BetSpammer;
@@ -103,6 +234,15 @@ ReturnsSpammer = (function(_super) {
 
   ReturnsSpammer.prototype.startSpamming = function() {
     ReturnsSpammer.__super__.startSpamming.apply(this, arguments);
+    $(document).on('ajaxSuccess', (function(_this) {
+      return function(a, b, c) {
+        if (!c.data) {
+          if (c.url === 'ajax/postToReturn.php') {
+            return _this.notify('Requested returns ready.');
+          }
+        }
+      };
+    })(this));
     return this.spamLounge();
   };
 
@@ -114,10 +254,11 @@ init = function() {
   var url;
   url = window.location.href;
   if (url.match('mybets') !== null) {
-    return new ReturnsSpammer;
+    new ReturnsSpammer;
   } else if (url.match('match') !== null) {
-    return new BetSpammer;
+    new BetSpammer;
   }
+  return new BotsChecker;
 };
 
 init();
